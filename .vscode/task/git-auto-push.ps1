92b56fded7106bae67535ab427d0160e4d97d5d6
Jerry Liu <liuyuwei1989@yeah.net>
2025-06-18 02:47:39 +0800

    我太牛逼了！

--- .vscode/task/git-auto-push.ps1
+++ .vscode/task/git-auto-push.ps1
@@ -1,47 +1,15 @@
 param(
-    [string]$commitMsg = "auto commit and push",
-    [string]$githubIp = ""
+    [string]$commitMsg = "auto commit and push"
 )
 $maxRetries = 30
 $retry = 0
-
-
-function Flush-DnsCache {
-    Write-Host 'Flushing DNS cache...'
-    try {
-        ipconfig /flushdns | Out-Null
-        Write-Host 'DNS cache flushed.'
-    } catch {
-        Write-Host 'Failed to flush DNS cache.'
-    }
-}
-
 git add .
 git commit -m "$commitMsg"
-
-function GitPush-WithCustomIp {
-    param(
-        [string]$ip
-    )
-    if ($ip -ne "") {
-        Write-Host "Temporarily set github.com to $ip for git push. (SSL verify disabled)"
-        git config --global url."https://$ip/".insteadOf "https://github.com/"
-        git -c http.sslVerify=false -c http.extraHeader="Host: github.com" push origin main
-        $result = $LASTEXITCODE
-        git config --global --unset url."https://$ip/".insteadOf
-        return $result
-    } else {
-        git push origin main
-        return $LASTEXITCODE
-    }
-}
-
 if ($LASTEXITCODE -ne 0) {
     Write-Host 'Nothing to commit. Try pushing any unpushed commits.'
     do {
-        Flush-DnsCache
-        $pushResult = GitPush-WithCustomIp $githubIp
-        if ($pushResult -eq 0) {
+        git push origin main
+        if ($LASTEXITCODE -eq 0) {
             Write-Host 'Push succeeded.'
             break
         } else {
@@ -55,9 +23,8 @@ if ($LASTEXITCODE -ne 0) {
     }
 } else {
     do {
-        Flush-DnsCache
-        $pushResult = GitPush-WithCustomIp $githubIp
-        if ($pushResult -eq 0) {
+        git push origin main
+        if ($LASTEXITCODE -eq 0) {
             Write-Host 'Push succeeded.'
             break
         } else {
